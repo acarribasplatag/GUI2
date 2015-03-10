5294516c90b05ed4a281534981328c0e34421d48
Bobby <bobbydonald25@gmail.com>
2015-03-09 20:09:09 -0400

    can be null

--- registration/models.py
+++ registration/models.py
@@ -6,8 +6,8 @@ from django.utils import timezone
 class UserProfile(models.Model):
     # This field is required. User Profile belongs to a user
     user = models.OneToOneField(User)
-    avatar = models.FileField(upload_to='profile/')
-    aboutMe = models.TextField()
-    interests = models.TextField()
+    avatar = models.FileField(upload_to='profile/', Field.null=True)
+    aboutMe = models.TextField(Field.null=True)
+    interests = models.TextField(Field.null=True)
     def __unicode__(self):              # __str__ on Python 3
         return self.user.username
