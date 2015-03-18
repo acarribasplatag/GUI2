40c9d618975af933bf45c843d3be6478e7600383
Bobby <bobbydonald25@gmail.com>
2015-03-17 22:14:15 -0400

    gitignore

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,13 +84,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_URL='/static/'
-STATIC_ROOT= 'http://www.cs.uml.edu/~vbalabha/static/'
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
