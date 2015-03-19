29fa4133c7c6466ef8c35c4a50b2f6fa0b86551a
Bobby <bobbydonald25@gmail.com>
2015-03-18 20:10:27 -0400

    fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,9 +86,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-
-STATIC_URL='/static/'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
