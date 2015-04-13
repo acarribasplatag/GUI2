298d01d3e1e02c1b7d6a7646205149f4032cdaab
Bobby <bobbydonald25@gmail.com>
2015-04-13 10:19:14 -0400

    error pages

--- polls/views.py
+++ polls/views.py
@@ -360,4 +360,16 @@ def contact_us(request):
 
     return render_to_response("contact_form/contact_form.html", args, RequestContext(request))
 
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
 
