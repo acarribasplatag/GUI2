5ba7ccd4cba0ed4bc331561835b95f5ca3c52213
Virinchi <Virinchi_Balabhadrapatruni@student.uml.edu>
2015-05-01 21:50:32 -0400

    Widgets comments

--- polls/widgets.py
+++ polls/widgets.py
@@ -1,5 +1,8 @@
 from django import forms
 
+# This was originally designed for entering poll choices, wherein choices were added to a list that was then saved.
+# This design was deemed to complex to implement within the time frame, so this form of input was removed.
+# However, this functionality may eventually return in a future release.
 class OptionsMultiWidget(forms.MultiWidget):
 
     def __init__(self, attrs=None):
